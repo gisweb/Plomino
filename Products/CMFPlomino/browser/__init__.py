9c8a49e814eee689d1d9ed25e9459c85a6c46528
Manuele Pesenti <manuele.pesenti@gmail.com>
2014-01-22 13:59:31 +0100

    a first generic commit

--- Products/CMFPlomino/browser/__init__.py
+++ Products/CMFPlomino/browser/__init__.py
@@ -1,2 +1,3 @@
+from bootstrapview import BootstrapView
 from zope.i18nmessageid import MessageFactory
 PlominoMessageFactory = MessageFactory('CMFPlomino')
--- Products/CMFPlomino/interfaces.py
+++ Products/CMFPlomino/interfaces.py
@@ -2,6 +2,15 @@
 from zope.interface.interface import Attribute
 from zope.interface import Interface
 
+class IBootstrapView(Interface):
+    """ """
+
+    def getColumnsClasses(view=None):
+        """ A helper method to return the clases for the columns of the site
+            it should return a dict with three elements:'one', 'two', 'content'
+            Each of them should contain the classnames for the first (leftmost)
+            second (rightmost) and middle column
+        """
 
 class IPlominoDatabase(Interface):
     """ Marker interface for .PlominoDatabase.PlominoDatabase
