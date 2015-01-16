499e8259566a1fbd58623c2439c6b7ec4cb46ec8
Marco Carbone <marco.carbone@gmx.com>
2015-01-16 08:51:18 +0100

    Update and rename PlominoEvents to plominoEvents.py

--- Products/CMFPlomino/plominoEvents.py
+++ Products/CMFPlomino/plominoEvents.py
@@ -2,14 +2,14 @@ from five import grok
 from zope.component.interfaces import IObjectEvent
 from zope.component.interfaces import ObjectEvent
 class IPlominoSaveEvent(IObjectEvent):
-pass
+  pass
 class PlominoSaveEvent(ObjectEvent):
-grok.implements(IPlominoSaveEvent)
-def __init__(self,object):
-self.object = object
+  grok.implements(IPlominoSaveEvent)
+  def __init__(self,object):
+    self.object = object
 class IPlominoCreateEvent(IObjectEvent):
-pass
+  pass
 class PlominoCreateEvent(ObjectEvent):
-grok.implements(IPlominoCreateEvent)
-def __init__(self,object):
-self.object = object
+  grok.implements(IPlominoCreateEvent)
+  def __init__(self,object):
+    self.object = object
