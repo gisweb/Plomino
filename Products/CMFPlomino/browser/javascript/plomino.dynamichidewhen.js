116803a6f45d6fc77299ba011660b18ba95466d8
vitolabbe <vito.labbe@hotmail.it>
2016-04-22 17:39:03 +0200

    simplehw in dialog

--- Products/CMFPlomino/browser/javascript/plomino.dynamichidewhen.js
+++ Products/CMFPlomino/browser/javascript/plomino.dynamichidewhen.js
@@ -21,9 +21,10 @@
 
     //Hidewhen without ajax call (only for single checkbox option)
     function simpleHidewhen() {
-        $(".hidewhen-" + $(this).attr('data-dhw')).toggle();
+        //$(".hidewhen-" + $(this).attr('data-dhw')).toggle();
+        hw = ".hidewhen-" + $(this).attr('data-dhw')
+        $(this).closest('form').find(hw).toggle();
     };
-
 
     $(function () {
         var elementName;
@@ -35,21 +36,5 @@
         $("input:checkbox[data-dhw]").not("[data-dhw = 1]").on("change",simpleHidewhen);
 
     });
-
-
-
-
-    //INIZIALIZZO TUTTO IL CONTENUTO DEL DIALOG DOPO AVER APERTO IL DIALOG DEL DATAGRID
-    $(document).on('opendialog',function(_, container){
-        var elementName;
-        $("input:checkbox[data-dhw = 1],input:radio[data-dhw = 1]").each(function(_,el){
-            elementName = $(el).attr("name");
-            $("input:checkbox[name = '" + elementName + "'],input:radio[name = '" + elementName + "']").on("change",refreshHidewhen);
-        })
-        $("input:text[data-dhw = 1],select[data-dhw = 1]").on("change",refreshHidewhen);
-        $("input:checkbox[data-dhw]").not("[data-dhw = 1]").on("change",simpleHidewhen);
-
-    });
-
 
 })(jQuery);
